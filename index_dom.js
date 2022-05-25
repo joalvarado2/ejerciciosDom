cfc83bbd60af66387182c9231c4b01aad469084d
joalvarado2 <jonathanalva92@gmail.com>
2022-05-24 19:34:41 -0500

    agregando el modo dark y el botton de scroll

--- index_dom.js
+++ index_dom.js
@@ -1,7 +1,9 @@
+import scrollTopButton from "./dom/boton_scroll.js";
 import countdown from "./dom/cuenta_regresiva.js";
 import hamburguerMenu from "./dom/menu_hamburguesa.js";
 import { digitalClock, alarm } from "./dom/reloj.js";
 import { moveBall, shortcuts } from "./dom/teclado.js";
+import darkTheme from "./dom/tema_oscuro.js";
 
 const d = document;
 d.addEventListener("DOMContentLoaded", (e) => {
@@ -9,9 +11,12 @@ d.addEventListener("DOMContentLoaded", (e) => {
   digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj");
   alarm("assets/mario.mp3", "#activar-alarma", "#desactivar-alarma");
   countdown("countdown", "sep 08,2022 00:00:00", "Feliz Cumpleaños");
+  scrollTopButton(".scroll-top-btn");
 });
 
 d.addEventListener("keydown", (e) => {
   shortcuts(e);
   moveBall(e, ".ball", ".stage");
 });
+
+darkTheme(".dark-theme-btn", "dark-mode");
