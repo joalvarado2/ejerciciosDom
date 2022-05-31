e1c615fceffaa8a6a55f72f234a38bb457d9f523
joalvarado2 <jonathanalva92@gmail.com>
2022-05-31 18:01:29 -0500

    agregando userAgent y responsive con js y envio de formulario con parametro

---
+++ dom/deteccion_dispositivos.js
@@ -0,0 +1,64 @@
+const d = document;
+const n = navigator;
+const ua = n.userAgent;
+
+export default function userDeviceInfo(id) {
+  const $id = d.getElementById(id);
+
+  const isMobile = {
+    android: () => ua.match(/android/i),
+    ios: () => ua.match(/iphone|ipad|ipod/i),
+    windows: () => ua.match(/windows phone/i),
+    any: function () {
+      return this.android() || this.ios() || this.windows();
+    },
+  };
+  const isDesktop = {
+    linux: () => ua.match(/linux/i),
+    mac: () => ua.match(/mac os/i),
+    windows: () => ua.match(/windows nt/i),
+    any: function () {
+      return this.linux() || this.mac() | this.windows();
+    },
+  };
+  const isBrowser = {
+    chrome: () => ua.match(/chrome/i),
+    safari: () => ua.match(/safari/i),
+    firefox: () => ua.match(/firefox/i),
+    opera: () => ua.match(/opera|opera mini/i),
+    edge: () => ua.match(/edge/i),
+    any: function () {
+      return (
+        this.chrome() ||
+        this.edge() ||
+        this.firefox() ||
+        this.opera() ||
+        this.safari()
+      );
+    },
+  };
+  // console.log(ua);
+  $id.innerHTML = `
+  <ul>
+    <li>User Agent: <b>${ua}</b></li>
+    <li>Plataforma: <b>${
+      isMobile.any() ? isMobile.any() : isDesktop.any()
+    }</b></li>
+    <li>Navegador: <b>${isBrowser.any()}</b></li>
+  </ul>
+`;
+
+  //Contenido exclusivo
+  if (isBrowser.chrome()) {
+    $id.innerHTML += `<p><mark>Este contenido se ve solo en chrome</mark></p>`;
+  }
+
+  if (isBrowser.edge()) {
+    $id.innerHTML += `<p><mark>Este contenido se ve solo en edge</mark></p>`;
+  }
+
+  //redirecciones
+  if (isMobile.android()) {
+    window.location.href = "https://reactjs.org/";
+  }
+}
--- index_dom.js
+++ index_dom.js
@@ -1,6 +1,9 @@
 import scrollTopButton from "./dom/boton_scroll.js";
 import countdown from "./dom/cuenta_regresiva.js";
+import userDeviceInfo from "./dom/deteccion_dispositivos.js";
 import hamburguerMenu from "./dom/menu_hamburguesa.js";
+import responsiveMedia from "./dom/objeto_responsive.js";
+import responsiveTester from "./dom/prueba_responsive.js";
 import { digitalClock, alarm } from "./dom/reloj.js";
 import { moveBall, shortcuts } from "./dom/teclado.js";
 import darkTheme from "./dom/tema_oscuro.js";
@@ -12,6 +15,21 @@ d.addEventListener("DOMContentLoaded", (e) => {
   alarm("assets/mario.mp3", "#activar-alarma", "#desactivar-alarma");
   countdown("countdown", "sep 08,2022 00:00:00", "Feliz Cumpleaños");
   scrollTopButton(".scroll-top-btn");
+  responsiveMedia(
+    "youtube",
+    "(min-width: 1024px)",
+    `<a href="https://www.youtube.com/watch?v=OfIfzVf8t6E&ab_channel=RamonesVEVO" target="_blank" rel="noopener">Ver Video</a>`,
+    `<iframe width="560" height="315" src="https://www.youtube.com/embed/HJWFsZ_YUc4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+  );
+  responsiveMedia(
+    "gmaps",
+    "(min-width: 1024px)",
+    `<a href="https://www.youtube.com/watch?v=OfIfzVf8t6E&ab_channel=RamonesVEVO" target="_blank" rel="noopener">Ver Mapa</a>`,
+    `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31815.814305443186!2d-74.23675442829236!3d4.598180813954895!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f758a17841789%3A0xc897578b55ac6342!2sCiudad%20Verde%2C%20Soacha%2C%20Cundinamarca!5e0!3m2!1ses!2sco!4v1654029286394!5m2!1ses!2sco" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>` 
+  );
+
+  responsiveTester("responsive-tester")
+  userDeviceInfo("user-device");
 });
 
 d.addEventListener("keydown", (e) => {
