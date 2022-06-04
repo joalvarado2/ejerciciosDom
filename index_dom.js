0cc5a05669f2aef51f1fb1dad4e1a6962483f0e6
joalvarado2 <jonathanalva92@gmail.com>
2022-06-03 19:32:01 -0500

    agregando ejercicio de deteccion de camara y geolocalizacion

--- index_dom.js
+++ index_dom.js
@@ -2,6 +2,8 @@ import scrollTopButton from "./dom/boton_scroll.js";
 import countdown from "./dom/cuenta_regresiva.js";
 import userDeviceInfo from "./dom/deteccion_dispositivos.js";
 import networkStatus from "./dom/deteccion_red.js";
+import webCam from "./dom/deteccion_webcam.js";
+import getGeolocation from "./dom/geolocalizacion.js";
 import hamburguerMenu from "./dom/menu_hamburguesa.js";
 import responsiveMedia from "./dom/objeto_responsive.js";
 import responsiveTester from "./dom/prueba_responsive.js";
@@ -26,11 +28,13 @@ d.addEventListener("DOMContentLoaded", (e) => {
     "gmaps",
     "(min-width: 1024px)",
     `<a href="https://www.youtube.com/watch?v=OfIfzVf8t6E&ab_channel=RamonesVEVO" target="_blank" rel="noopener">Ver Mapa</a>`,
-    `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31815.814305443186!2d-74.23675442829236!3d4.598180813954895!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f758a17841789%3A0xc897578b55ac6342!2sCiudad%20Verde%2C%20Soacha%2C%20Cundinamarca!5e0!3m2!1ses!2sco!4v1654029286394!5m2!1ses!2sco" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>` 
+    `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31815.814305443186!2d-74.23675442829236!3d4.598180813954895!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f758a17841789%3A0xc897578b55ac6342!2sCiudad%20Verde%2C%20Soacha%2C%20Cundinamarca!5e0!3m2!1ses!2sco!4v1654029286394!5m2!1ses!2sco" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`
   );
 
-  responsiveTester("responsive-tester")
+  responsiveTester("responsive-tester");
   userDeviceInfo("user-device");
+  webCam("webcam");
+  getGeolocation("geolocation");
 });
 
 d.addEventListener("keydown", (e) => {
